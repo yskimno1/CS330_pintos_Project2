49ab6eea866dc2649c1d1a94d3662249b3e5b89c
Julia510 <Julia510@kaist.ac.kr>
2019-04-01 04:39:43 +0900

    1

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -42,7 +42,7 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
   void* if_esp = f->esp;
   if(is_kernel_vaddr(if_esp)){ // have to change yunseong
