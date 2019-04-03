a4cd1ad30d571094d266690b9808f2f6a371535e
윤성 <yskimno1@gmail.com>
2019-04-03 17:25:55 +0900

    exit added

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -155,7 +155,7 @@ page_fault (struct intr_frame *f)
 //     return;
 //   }
 //   thread_exit();
-
+  exit(-1);
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
