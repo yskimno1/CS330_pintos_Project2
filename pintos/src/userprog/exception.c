9aee73331c67fa8c30af2b145380e79a7e2bda12
윤성 <yskimno1@gmail.com>
2019-04-04 17:45:08 +0900

    typo

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -5,6 +5,7 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "userprog/syscall.h"
+#include "threads/vaddr.h"
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
