64887cb44e1c6ae9bda67292d8d2a74479125d07
윤성 <yskimno1@gmail.com>
2019-04-04 17:44:09 +0900

    modified page fault handling

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -156,7 +156,7 @@ page_fault (struct intr_frame *f)
 //   }
 //   thread_exit();
 //   printf("memory : %X\n", *(int *)(f->esp));
-  exit(-1);
+  if(!user || is_kernel_vaddr(fault_addr)) exit(-1);
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
