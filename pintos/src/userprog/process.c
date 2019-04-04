93e995d0a9217a2bd7b85a9553518d1eee081f26
윤성 <yskimno1@gmail.com>
2019-04-04 16:41:16 +0900

    typo

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -162,7 +162,7 @@ process_wait (tid_t child_tid)
   list_remove(&th_child->elem_list_children);
   status = th_child->exit_status;
   sema_up(&thread_current()->sema_exited);
-  printf("process wait done, status %d with pid %d\n", status, pid);
+  printf("process wait done, status %d with pid %d\n", status, child_pid);
   return status;
 }
 
