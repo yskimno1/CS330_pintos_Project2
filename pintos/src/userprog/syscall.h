3aee54d02c7498e5ae022fa04336016e7a2356d6
Julia510 <Julia510@kaist.ac.kr>
2019-04-04 14:01:23 +0900

    struct file added

--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -1,6 +1,13 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+struct file 
+  {
+    struct inode *inode;        /* File's inode. */
+    off_t pos;                  /* Current position. */
+    bool deny_write;            /* Has file_deny_write() been called? */
+  };
+  
 void syscall_init (void);
 void exit (int status);
 
