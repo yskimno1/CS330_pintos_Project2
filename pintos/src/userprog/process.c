0b48724af6ee941fe65f98844dfc8ffef943f65d
윤성 <yskimno1@gmail.com>
2019-04-05 11:42:40 +0900

    roll back rox

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -404,7 +404,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
   *eip = (void (*) (void)) ehdr.e_entry;
 
   success = true;
-  file_deny_write(file);
+  // file_deny_write(file);
  done:
   /* We arrive here whether the load is successful or not. */
   file_close (file);
