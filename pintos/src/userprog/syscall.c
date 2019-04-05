07a7f03e5b81b124c4f2e6d12c32cf3dda453d88
윤성 <yskimno1@gmail.com>
2019-04-05 11:34:52 +0900

    test

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -199,13 +199,6 @@ halt (void){
 	power_off();
 }
 
-void
-exit_pagefault (int status){
-	struct thread* t = thread_current();
-	t->exit_status = status;
-	
-}
-
 void 
 exit (int status){
   struct thread* t = thread_current();
@@ -272,8 +265,8 @@ int open (const char *file){
   struct thread *t = thread_current();
   int fd = (t->fd_vld)++;
   t->fdt[fd] = f;
-  if (!strcmp(t->name, file)) 
-      // file_deny_write(f);
+  // if (!strcmp(t->name, file)) 
+  //     file_deny_write(f);
   filelock_release();
   return fd; 
 }
